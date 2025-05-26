b03bbf035d2b7530e64de55f9d20925516b1548b
mumuvrf <viniciusrf2@al.insper.edu.br>
2025-05-26 01:07:13 -0300

    Fix script get_commits.py : change sources and translate commit messages to english

--- get_commits.py
+++ get_commits.py
@@ -1,5 +1,6 @@
 import os
 from github import Github
+from deep_translator import GoogleTranslator
 import pandas as pd
 
 from dotenv import load_dotenv, find_dotenv
@@ -12,22 +13,24 @@ g = Github(GITHUB_KEY)
 repositories = ["mumuvrf/CoDes-App", "marcosnovaesq/qual_a_boua",
                 "mumuvrf/PyGameRPG", "apracadev/praca", "talesitf/cautious-enigma",
                 "talesitf/Codes-221", "talesitf/DataDuner", 
-                "arcotech-services/data-analysis-rag-experiments"]
+                "pedronery07/Pygame-2022.2", "antoniolma/PyGameDessoft",
+                "raulikeda/OpenManus"]
 
-commit_data = [["code", "message", "is_code_related"]]
+commit_data = [["code", "message", "message_english", "is_code_related"]]
 
 for repo_name in repositories:
     print(f"Opa! Tô pegando commits do repositório: {repo_name}")
     repo = g.get_repo(repo_name)
     for commit in repo.get_commits():
         message = commit.commit.message
+        message_english = GoogleTranslator(source='auto', target='en').translate(message)
         full_commit = repo.get_commit(commit.sha)
         code = ""
         for file in full_commit.files:
             filename = file.filename
             patch = file.patch
             code += f"\nFile:{filename}\nCode modifications:\n{patch}\n"
-        commit_data.append([code, message])
+        commit_data.append([code, message, message_english])
 
 df = pd.DataFrame(commit_data)
 df.to_excel("data.xlsx")
